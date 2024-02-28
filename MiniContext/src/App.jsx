65e8e363480902262fadd9fde7d9ce35733ca5ef
AbirMondal1234 <69684752+AbirMondal1234@users.noreply.github.com>
2024-02-28 10:50:41 +0530

    update

--- MiniContext/src/App.jsx
+++ MiniContext/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import UserContextProvider from './context/UserContextProv'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>HIiiii</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
