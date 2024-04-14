7131144796f16879caf048177373f5704e403eab
AbirMondal1234 <69684752+AbirMondal1234@users.noreply.github.com>
2024-04-15 00:48:51 +0530

    done

--- ReduxtollToDo/src/App.jsx
+++ ReduxtollToDo/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddToDo'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <>
      <h1>HIii</h1>
+     <AddTodo/>
+     <Todo/>
      
     </>
   )
